ce0545761e5e146ed2917ebb0435cad72e3a4579
Mohammad Wahiduzzaman <mwahiduz@my.centennialcollege.ca>
2021-10-28 23:10:16 -0400

    MongoDB Atlas connected

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books"
+  //"URI": "mongodb://localhost/books"
+  "URI": "mongodb+srv://shamim:1234@cluster0.3wusp.mongodb.net/books?retryWrites=true&w=majority"
 };
